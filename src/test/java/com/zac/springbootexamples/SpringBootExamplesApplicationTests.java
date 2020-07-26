40dde777baefda5b998b86fb98cab9fbe8dbdd88
redcobweb <msmwwscz@outlook.com>
2020-07-26 16:07:06 +0800

    项目创建

---
+++ src/test/java/com/zac/springbootexamples/SpringBootExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zac.springbootexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
